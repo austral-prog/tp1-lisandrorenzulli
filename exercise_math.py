7b019f0db0159464305764ecb4b8cba5e172dd90
lisandrorenzulli <lisandrorenzulli@gmail.com>
2025-08-21 10:21:45 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    print(suma)
+    resta = a - b
+    print(resta)
+    producto = a * b
+    print(producto)
+    promedio = (a + b)/2
+    print(promedio)
+    cociente = a // b
+    print(cociente)
+    resto = a % b
+    print(resto)
+    valor = a / b
+    print(valor)
+math()
